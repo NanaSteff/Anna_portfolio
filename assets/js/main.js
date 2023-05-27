159118981e20b77193f2abb67d73a0575f40864c
Anna Stefano Narivelomanana <annanarivelomanana07@gmail.com>
2023-05-27 02:43:46 +0100

    add ITprojet and AboutMe sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,26 @@
 function showContent(name) {
     const page = document.getElementsByClassName('content');
-    for (let i=0; i<page.length;i++){
-        if (page[i].id==name){
-            if (page[i].classList.contains('d-none')){
+    const bg = document.getElementById('status-bg')
+    for (let i = 0; i < page.length; i++) {
+        if (page[i].id == name) {
+            if (page[i].classList.contains('d-none')) {
                 page[i].classList.remove('d-none')
+                if (name == "status") {
+                    if (bg.classList.contains("d-none")) {
+                        bg.classList.remove("d-none")
+                    }
+                }
+                else {
+                    if (!bg.classList.contains("d-none")) {
+                        bg.classList.add("d-none")
+                    }
+                }
             }
         }
-        else{
-            if (!page[i].classList.contains('d-none')){
+        else {
+            if (!page[i].classList.contains('d-none')) {
                 page[i].classList.add('d-none')
             }
         }
-    } 
+    }
 }
